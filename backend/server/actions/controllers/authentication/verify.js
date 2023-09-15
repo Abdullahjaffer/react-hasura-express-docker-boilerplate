0b27b1d545d9e416fd3ae5946700135db6f4f3ff
Abdullah Jaffer <abdullahjaffer7@gmail.com>
2023-09-16 00:58:58 +0500

    feat: updated backend .env

--- backend/server/actions/controllers/authentication/verify.js
+++ backend/server/actions/controllers/authentication/verify.js
@@ -4,7 +4,7 @@ import {
 	AuthorizationHeader,
 	HASURA_ROLE_ANONYMOUS,
 	XHasuraRole,
-	XHasuraUserID,
+	XHasuraUserID
 } from "../../../utils/constants/auth.js";
 import { verifyToken } from "../../../utils/jwt.js";
 
@@ -14,20 +14,7 @@ async function verifyController(req, res) {
 	const anonymous = {
 		[XHasuraRole]: HASURA_ROLE_ANONYMOUS,
 	};
-
-	// const childLogger = logger.child({
-	// 	type: "auth",
-	// });
-
-	// TODO: verify token
 	if (!token) {
-		// childLogger.info("authorization header is empty", {
-		// 	request_headers: env.DEBUG ? req.headers : {},
-		// 	session_variables: anonymous,
-		// 	http_code: 200,
-		// 	latency: new Date().getTime() - start.getTime(),
-		// });
-
 		return res.json(anonymous);
 	}
 
@@ -35,13 +22,6 @@ async function verifyController(req, res) {
 
 	try {
 		if (parts.length < 2 || parts[0] !== AuthBearer) {
-			// childLogger.warn("invalid authorization token", {
-			// 	request_headers: req.headers,
-			// 	session_variables: anonymous,
-			// 	http_code: 200,
-			// 	latency: new Date().getTime() - start.getTime(),
-			// });
-
 			return res.json(anonymous);
 		}
 
@@ -50,43 +30,17 @@ async function verifyController(req, res) {
 		const user = await getUserById(decodedToken.user.id);
 
 		if (!user) {
-			// childLogger.warn("user is not found or deleted", {
-			// 	request_headers: req.headers,
-			// 	session_variables: anonymous,
-			// 	http_code: 200,
-			// 	latency: new Date().getTime() - start.getTime(),
-			// });
-
 			return res.json(anonymous);
 		}
 		const jsonData = {
 			[XHasuraUserID]: user.id,
 			[XHasuraRole]: user.role,
 		};
-
-		// childLogger.info("finish authentication", {
-		// 	// request_headers: env.DEBUG ? req.headers : {},
-		// 	// session_variables: jsonData,
-		// 	// http_code: 200,
-		// 	// latency: new Date().getTime() - start.getTime(),
-		// });
-
 		console.log(jsonData);
 
 		return res.json(jsonData);
 	} catch (err) {
 		console.log(err);
-		// childLogger.error("authenticated failure", {
-		// 	request_headers: req.headers,
-		// 	session_variables: anonymous,
-		// 	http_code: 200,
-		// 	latency: new Date().getTime() - start.getTime(),
-		// 	error: {
-		// 		message: err.message,
-		// 		...err,
-		// 	},
-		// });
-
 		return res.json(anonymous);
 	}
 }
